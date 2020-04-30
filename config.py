c909b6ce8863862c765989c046378af5a38fd851
Flako12345 <emil.thudium@gmail.com>
2020-04-30 20:51:09 +0200

    Added filename and normalization to sourdough readings. Fixed config handling and cleanup

--- config.py
+++ config.py
@@ -13,14 +13,14 @@ class Config:
 
 
 class DevelopmentConfig(Config):
-   # SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
     DEBUG = True
     SECRET_KEY = 'RandomSecret'
 
 class ProductionConfig(Config):
-    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')#'mysql://sourdoughadmin@sourdoughmonitor01:ClintYeastwood2019@sourdoughmonitor01.mysql.database.azure.com/sourdoughmonitor01'#os.getenv('SQLALCHEMY_DATABASE_URI', 'mysql://sourdoughadmin@sourdoughmonitor01:ClintYeastwood2019@sourdoughmonitor01.mysql.database.azure.com/sourdoughmonitor01')
+    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
     DEBUG = False
-    SECRET_KEY = os.getenv('SECRET_KEY')#'SecretKeyisSecret' #  os.getenv('SECRET_KEY', 'SomeSecret')
+    SECRET_KEY = os.getenv('SECRET_KEY')
 
 class TestConfig(Config):
     pass
