7bc20af15ffa82f4a09ad21fda2bde67d9dfd2bc
Flako12345 <emil.thudium@gmail.com>
2020-05-06 06:58:56 +0200

    Added pool size to Config

--- config.py
+++ config.py
@@ -6,6 +6,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY' 'SomeSecret')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_POOL_SIZE = os.getenv('SQLALCHEMY_POOL_SIZE', 119)
 
     @staticmethod
     def init_app(app):
